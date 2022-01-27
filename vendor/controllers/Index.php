e1e097eb210fabee2b440fd154d3b8c6f9ef212c
Artem Stasenko <1403060@gmail.com>
2022-01-27 16:52:19 +0200

    construct created in index.php

--- vendor/controllers/Index.php
+++ vendor/controllers/Index.php
@@ -1,7 +1,22 @@
 <?php
-
+namespace controllers;
+use View;
+use Task;
 
 class Index
 {
+    /**
+     * @var View
+     */
+    private $view;
+    /**
+     * @var Task
+     */
+    private $task;
 
+    public function __construct()
+    {
+        $this->task = new Task();
+        $this->view = new View();
+    }
 }
