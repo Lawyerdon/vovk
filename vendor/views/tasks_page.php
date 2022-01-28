073c9a1eaab5c9d022e13b6300b37bd255e8d91c
Анна Гошкодер <96648236+Alex251221@users.noreply.github.com>
2022-01-28 17:38:06 +0200

    create tasks_page.php

--- vendor/views/tasks_page.php
+++ vendor/views/tasks_page.php
@@ -1,1 +1,23 @@
-<?php
+
+<h1>Tasks page</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>№</th>
+            <th>Tansk</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php if(count($tasks) > 0):?>
+    <?php foreach($tasks as $task):?>
+        <tr>
+            <td><?= $task['id']?></td>
+            <td><?= $task['name']?></td>
+        </tr>
+    <?php endforeach;?>
+    <?php endif;?>
+    </tbody>
+
+
+</table>
