3c604ac6c8454ec4a84b0a98765ab66b2fa026ea
Artem Stasenko <1403060@gmail.com>
2022-01-28 13:14:47 +0200

    store created

--- vendor/controllers/Index.php
+++ vendor/controllers/Index.php
@@ -25,6 +25,8 @@ class Index
         $this->view->render('create');
     }
     public function store(){
-
+        $id = filter_input(INPUT_POST, 'id');
+        $this->task->deleteById($id);
+        Route::redirect(Route::url('index', 'index'));
     }
 }
