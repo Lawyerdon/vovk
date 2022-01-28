812bb1449bc547ae6309827e2fcd7856817c8e32
the-vladislav <test@test.com>
2022-01-28 16:09:38 +0200

    created edit page

--- vendor/views/edit_page.php
+++ vendor/views/edit_page.php
@@ -1,1 +1,6 @@
-<?php
+<form action="<?php Route::url("index", "edit"); ?>">
+    <label for="article">Article:</label>
+    <input type="text" name="article" id="article" autofocus/>
+    <input type="hidden" name="id" value="<?= $_POST["forChange"]; ?>"/>
+    <input type="submit" value="Change value"/>
+</form>
