66f1678a170d5b6e3053be63f8103381f326378d
mirosh9719 <osu.eagle@gmail.com>
2022-01-28 17:51:26 +0200

    DB

--- vendor/db/Task.php
+++ vendor/db/Task.php
@@ -1,7 +1,38 @@
 <?php
 
+namespace db;
 
 class Task
 {
+    private $connect;
+
+    public function __construct()
+    {
+        $connect = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    }
+
+    public function all()
+    {
+        if ($this->connect->connect_errno != 0) {
+            exit($this->connect->connect_error);
+        }
+
+        $query = "SELECT * FROM tasks;";
+        $result = $this->connect->query($query);
+
+        if (!$result) {
+            //TODO log of error
+            return null;
+        }
+
+        $tasks = $result->fetch_all(MYSQLI_ASSOC);
+
+        return $tasks;
+    }
+
+    public function add()
+    {
+
+    }
 
 }
