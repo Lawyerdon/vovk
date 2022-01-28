98779d24f855205e6495050fec6731a3eaf6d9e0
the-vladislav <test@test.com>
2022-01-28 15:16:37 +0200

    created class View

--- vendor/core/View.php
+++ vendor/core/View.php
@@ -3,5 +3,17 @@
 
 class View
 {
+    private $template;
+    private $page;
 
+    public function __construct($page, $template = "default")
+    {
+        $this->page = $page;
+        $this->template = $template;
+    }
+
+    public function render($page, array $data = []) {
+        extract($data);
+        include_once "vendor" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . $this->template . ".php";
+    }
 }
