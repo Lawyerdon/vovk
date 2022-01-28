66b245115e23db603b8430dd59d936b8a2feeba4
the-vladislav <test@test.com>
2022-01-28 16:22:17 +0200

    remaked edit page

--- vendor/views/edit_page.php
+++ vendor/views/edit_page.php
@@ -1,6 +1,6 @@
 <form action="<?php Route::url("index", "edit"); ?>">
     <label for="article">Article:</label>
-    <input type="text" name="article" value="<?= $data["task"] ?>" id="article" autofocus/>
-    <input type="hidden" name="id" value="<?= $data["id"] ?>"/>
+    <input type="text" name="article" value="<?= $task["article"] ?>" id="article" autofocus/>
+    <input type="hidden" name="id" value="<?= $task["id"] ?>"/>
     <input type="submit" value="Change value"/>
 </form>
