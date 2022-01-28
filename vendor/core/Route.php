5f041dea8c05ccb2f26a0d00a938b0d51b4628c7
Анна Гошкодер <96648236+Alex251221@users.noreply.github.com>
2022-01-28 16:17:52 +0200

    Revert "create init, redirect, url, error404 to Route"
    This reverts commit b3fbeae4

--- vendor/core/Route.php
+++ vendor/core/Route.php
@@ -3,46 +3,5 @@
 
 class Route
 {
-    static public function init() {
-        $URIs = explode('/', $_SERVER['REQUEST_URI']);
-        array_shift($URIs);
 
-        $controllerName = 'index';
-        $actionName = 'index';
-        if(!empty($URIs[0])) {
-            $controllerName = mb_convert_case(mb_strtolower($URIs[0]), MB_CASE_TITLE);
-        }
-        if(!empty($URIs[1])) {
-            $actionName = mb_strtolower($URIs[1]);
-        }
-        $controllerClassName = '\controllers\\'.$controllerName.'Controller';
-        var_dump($controllerClassName);
-
-        if(!class_exists($controllerClassName)) {
-            self::error404();
-        }
-
-        $controller = new $controllerClassName();
-        if(!method_exists($controller, $actionName)) {
-            self::error404();
-        }
-        $controller->$actionName();
-    }
-    static public function redirect($url = null)
-    {
-        if(!isset($url)) {
-            return $url;
-        } else {
-            return $_SERVER['PHP_SELF'];
-        }
-        header('Location:'.$url);
-    }
-    static public function url($controllerName = 'index', $actionName = 'index')
-    {
-        return '/'.$controllerName.'/'.$actionName;
-    }
-    static public function error404() {
-        http_response_code(404);
-        exit();
-    }
 }
