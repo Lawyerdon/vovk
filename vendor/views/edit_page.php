2fb27b2b164b2c03fc88beab451a78d7925745fe
the-vladislav <test@test.com>
2022-01-31 16:53:13 +0200

    fixed bugs in create_page and edit_page

--- vendor/views/edit_page.php
+++ vendor/views/edit_page.php
@@ -1,4 +1,4 @@
-<form action="<?php Route::url("index", "edit"); ?>">
+<form action="<?php Route::url("index", "edit"); ?>" method="post">
     <label for="article">Article:</label>
     <input type="text" name="article" value="<?= $task["article"] ?>" id="article" autofocus/>
     <input type="hidden" name="id" value="<?= $task["id"] ?>"/>
